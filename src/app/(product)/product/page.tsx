0cd9d90dd612f756e06bf7b032646c74b1f2fb82
Emre Erden <emreerden@pm.me>
2023-12-27 00:28:44 +0300

    add product layout and page

--- src/app/(product)/product/page.tsx
+++ src/app/(product)/product/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomeComponent from '@/components/company/pages/home'
+import HomeComponent from '@/components/product/pages/home'
 
 type Props = {}
 
--- src/app/(product)/product/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const Product = (props: Props) => {
-  return <div>Product Profile</div>
-}
-
-export default Product
