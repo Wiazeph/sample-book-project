6e73252449a0131c8acf9ab2d3475a54f897cfc3
Emre Erden <emreerden@pm.me>
2023-12-31 23:38:15 +0300

    edit supabase client

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,5 +1,7 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+import type { CookieOptions } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
