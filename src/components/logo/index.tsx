febea51d22e44e915a54f210b2b0ba5bb4843509
Emre Erden <emreerden@pm.me>
2024-01-02 16:36:09 +0300

    add pathname to logo component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 type Props = {}
 
 const Logo = (props: Props) => {
+  const pathname = usePathname()
+
   return (
     <div className="Logo shrink-0 uppercase">
-      <Link href="/">-Logo-</Link>
+      <Link href={pathname !== '/' ? '/' : ''}>-Logo-</Link>
     </div>
   )
 }
